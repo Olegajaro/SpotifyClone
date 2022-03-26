4dd4878565292e7fdaac2bc8180886f55ac4be12
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-26 21:57:01 +0300

    setup: Create new Xcode project without storyboard

---
+++ SpotifyClone/SpotifyClone/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  SpotifyClone
+//
+//  Created by Олег Федоров on 26.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        
+        let navigationContoller = UINavigationController(
+            rootViewController: ViewController()
+        )
+        window?.rootViewController = navigationContoller
+        
+        return true
+    }
+}
+
---
+++ SpotifyClone/SpotifyClone/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Олег Федоров on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
