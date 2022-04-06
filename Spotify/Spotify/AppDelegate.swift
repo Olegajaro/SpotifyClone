19980a92e6577a1b7e50994e609845f15173a1bf
Oleg Fedorov <olegajaro94@gmail.com>
2022-04-06 18:03:46 +0300

    feat: Spotify Auth Set Up

--- Spotify/Spotify/AppDelegate.swift
+++ Spotify/Spotify/AppDelegate.swift
@@ -32,6 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = window
         
+//        print("DEBUG: \(AuthService.shared.signInURL?.absoluteString)")
+        
         return true
     }
 }
--- Spotify/Spotify/Services/AuthService.swift
+++ Spotify/Spotify/Services/AuthService.swift
@@ -11,7 +11,21 @@ final class AuthService {
     
     static let shared = AuthService()
     
+    struct Constans {
+        static let cliendID = "a138ef3c121b46a2914092328c256da9"
+        static let clientSecret = "0ad2572a341641959e84a73d5d2e3399"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let stringURL = "\(baseURL)?response_type=code&client_id=\(Constans.cliendID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: stringURL)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -31,5 +45,19 @@ final class AuthService {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String,
+                                     completion: @escaping (Bool) -> Void) {
+         // Get token
+        
+    }
+    
+    private func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
